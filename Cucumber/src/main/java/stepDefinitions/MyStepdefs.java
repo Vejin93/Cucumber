5d5c20e891ec858e47e72ad5d0161e3f1795eecc
Vladimir Vejin <vejin.vladimir@gmail.com>
2023-01-29 22:03:22 +0100

    Add MyStepDefs class - Step definition class from Login.feature

---
+++ Cucumber/src/main/java/stepDefinitions/MyStepdefs.java
@@ -0,0 +1,33 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.*;
+
+public class MyStepdefs {
+    @Given("User Launch browser")
+    public void userLaunchBrowser() {
+    }
+
+    @And("opens URL {string}")
+    public void opensURL(String arg0) {
+    }
+
+    @When("User navigate to MyAccount menu")
+    public void userNavigateToMyAccountMenu() {
+    }
+
+    @And("click on Login")
+    public void clickOnLogin() {
+    }
+
+    @And("User enter Email as {string} and Password as {string}")
+    public void userEnterEmailAsAndPasswordAs(String arg0, String arg1) {
+    }
+
+    @And("click on Login button")
+    public void clickOnLoginButton() {
+    }
+
+    @Then("User navigates to MyAccount Page")
+    public void userNavigatesToMyAccountPage() {
+    }
+}
